a4ea13a41414b269f15fe7c86e46df5c94b2bf25
fernandezdlg <juanfernandezdlg@gmail.com>
2024-08-07 20:27:03 -0600

    typo corrected

--- app/latticeField_tests/print_tests.cpp
+++ app/latticeField_tests/print_tests.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    LatticeField<float> latticeFloat(4, 4, 4, 4, 0.0);
+    LatticeField<double> latticeFloat(4, 4, 4, 4, 0.0);
     std::cout << "latticeFloat(0,1,2,3) : " << latticeFloat(0,1,2,3) << std::endl;
 
     LatticeField<double> latticeDouble(4, 4, 4, 4, 1.0);
